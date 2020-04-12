b74b8375452eb3adf29f83b9208fb54b85258a3f
ROHIT SINGLA <rohitsingla2611@gmail.com>
2020-04-13 03:30:06 +0530

    UPDATE

--- app/src/main/java/rohitsingla/rdrock/tictactoe/MainActivity.java
+++ app/src/main/java/rohitsingla/rdrock/tictactoe/MainActivity.java
@@ -1,14 +1,29 @@
 package rohitsingla.rdrock.tictactoe;
 
+import android.os.Bundle;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    long backKeyPressedTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public void onBackPressed() {
+        if (backKeyPressedTime + 2000 > System.currentTimeMillis()) {
+            super.onBackPressed();
+            return;
+        } else {
+            Toast.makeText(this, "Press again to Exit", Toast.LENGTH_SHORT).show();
+        }
+        backKeyPressedTime = System.currentTimeMillis();
+    }
+
 }
